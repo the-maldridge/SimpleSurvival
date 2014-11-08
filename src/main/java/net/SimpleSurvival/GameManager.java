c150e640b05ca620a62bf654e95d843c5ace53ab
Michael Aldridge <aldridge.mac@gmail.com>
2014-11-08 14:37:30 -0600

    initial commit

---
+++ src/main/java/net/SimpleSurvival/GameManager.java
@@ -0,0 +1,11 @@
+package net.SimpleSurvival;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by maldridge on 10/21/14.
+ */
+public class GameManager extends JavaPlugin {
+
+}
---
+++ src/main/java/net/SimpleSurvival/SpawnManager.java
@@ -0,0 +1,10 @@
+package net.SimpleSurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by maldridge on 10/21/14.
+ */
+public class SpawnManager {
+
+}
