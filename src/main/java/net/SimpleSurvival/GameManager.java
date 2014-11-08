e9a442fd90e7f73d7fcdabd507f1458f920bfe20
geppettodivacin <ericdilmore@gmail.com>
2014-11-08 17:23:06 -0600

    Add addSpawn and setSpawn commands.

--- src/main/java/net/SimpleSurvival/GameManager.java
+++ src/main/java/net/SimpleSurvival/GameManager.java
@@ -1,11 +1,60 @@
 package net.SimpleSurvival;
 
+import org.bukkit.entity.Player;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.Location;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import java.util.ArrayList;
+
+import net.SimpleSurvival.SpawnManager;
 
 /**
  * Created by maldridge on 10/21/14.
  */
 public class GameManager {
+	private SpawnManager spawnManager = new SpawnManager();
+	private CommandParser commandParser = new CommandParser();
 
+	private class CommandParser {
+		public boolean parseCommand(CommandSender sender, Command cmd, String label, String[] args) {
+			if(cmd.getName().equalsIgnoreCase("setSpawn")) {
+				return setSpawn((Player)sender, cmd, label, args);
+			} else if(cmd.getName().equalsIgnoreCase("addSpawn")) {
+				return addSpawn((Player)sender, cmd, label, args);
+			}
+			return false;
+		}
+
+		private boolean setSpawn(Player sender, Command cmd, String label, String[] args) {
+			if(args.length != 1) return false;
+
+			int spawnNum;
+			try {
+				spawnNum = Integer.valueOf(args[0]);
+			} catch (NumberFormatException e) {
+				return false;
+			}
+
+			ArrayList<Location> spawnPoints = spawnManager.getSpawnList();
+
+			if(spawnPoints.size() < spawnNum) {
+				sender.sendMessage("There are not " + spawnNum + " spawn points yet.");
+			} else
+			{
+				spawnPoints.set(spawnNum, sender.getLocation());
+			}
+			return true;
+		}
+
+		private boolean addSpawn(Player sender, Command cmd, String label, String[] args) {
+			if(args.length != 0) return false;
+
+			ArrayList<Location> spawnPoints = spawnManager.getSpawnList();
+
+			spawnPoints.add(sender.getLocation());
+
+			return true;
+		}
+	}
 }
