3c5d3eda5294b060dc51ee333f33d75c439c8e52
Michael Aldridge <aldridge.mac@gmail.com>
2014-11-08 15:26:48 -0600

    add requisite files for plugin.yml

--- src/main/java/net/SimpleSurvival/GameManager.java
+++ src/main/java/net/SimpleSurvival/GameManager.java
@@ -6,6 +6,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 /**
  * Created by maldridge on 10/21/14.
  */
-public class GameManager extends JavaPlugin {
+public class GameManager {
 
 }
---
+++ src/main/java/net/SimpleSurvival/SimpleSurvival.java
@@ -0,0 +1,10 @@
+package net.SimpleSurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by maldridge on 11/8/14.
+ */
+public class SimpleSurvival extends JavaPlugin {
+
+}
