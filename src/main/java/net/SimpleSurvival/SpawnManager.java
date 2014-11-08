8d479438df2e075333f93c91fd55353d06161a20
geppettodivacin <ericdilmore@gmail.com>
2014-11-08 14:58:41 -0600

    Create an ArrayList for spawnpoints.

--- src/main/java/net/SimpleSurvival/SpawnManager.java
+++ src/main/java/net/SimpleSurvival/SpawnManager.java
@@ -1,10 +1,12 @@
 package net.SimpleSurvival;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Location;
+import java.util.ArrayList;
 
 /**
  * Created by maldridge on 10/21/14.
  */
 public class SpawnManager {
-
+	ArrayList<Location> spawnPointList = new ArrayList<Location>();
 }
