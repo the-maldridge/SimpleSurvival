06ef68e7bc95697f9a3a9b1d55b86a885bb0eea4
Michael Aldridge <aldridge.mac@gmail.com>
2014-11-09 15:53:49 -0600

    move settings classes to a package

--- src/main/java/net/SimpleSurvival/SimpleSurvival.java
+++ src/main/java/net/SimpleSurvival/SimpleSurvival.java
@@ -1,11 +1,11 @@
 package net.SimpleSurvival;
 
+import net.SimpleSurvival.settings.WorldSettings;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.World;
-import org.bukkit.Server;
 
 import java.util.HashMap;
 
