1337fba3870fb0f2e5e5bea067293b8898550e24
geppettodivacin <ericdilmore@gmail.com>
2014-11-08 19:30:03 -0600

    Add WorldSettings class.

---
+++ src/main/java/net/SimpleSurvival/WorldSettings.java
@@ -0,0 +1,29 @@
+package net.SimpleSurvival;
+
+import org.bukkit.Location;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class WorldSettings {
+	/**
+	 * Minimum number of players required for this world.
+	 */
+	public int minPlayers;
+	/**
+	 * Maximum number of players able to play in this world.
+	 */
+	public int maxPlayers;
+	/**
+	 * List of spawn points.
+	 */
+	public ArrayList<Location> spawns;
+	/**
+	 * Hash of loot with item name as key and frequency as the value.
+	 */
+	public HashMap<String, Double> loot;
+	/**
+	 * List of breakable blocks.
+	 */
+	public ArrayList<String> breakables;
+}
