78e5915a7f57454eab3a1aa60e8ea7daa940f7c4
Michael Aldridge <aldridge.mac@gmail.com>
2014-11-09 20:25:14 -0600

    alter world copying code

--- src/main/java/net/SimpleSurvival/WorldManager.java
+++ src/main/java/net/SimpleSurvival/WorldManager.java
@@ -1,7 +1,11 @@
 package net.SimpleSurvival;
 
 
+import org.bukkit.Bukkit;
+
 import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 /**
  * Created by maldridge on 11/8/14.
@@ -13,85 +17,39 @@ public class WorldManager {
         this.plugin = plugin;
     }
 
-    public void importWorlds() {
+    public void newWorldFromTemplate(String template, String newName) {
         boolean errors = false;
-        File backupDir = new File(this.plugin.getDataFolder(), "backups");
-        for(File source : backupDir.listFiles()) {
-            if(source.isDirectory()) {
-                File target = new File(this.plugin.getServer().getWorldContainer(), source.getName());
-                if(target.exists() && target.isDirectory()) { //delete the old world folder
-                    if(!delete(target)) {
-                        plugin.getLogger().severe("Failed to reset world \"" + source.getName() + "\" - could not delete old " +
-                                "world folder.");
-                        errors = true;
-                        continue;
-                    }
-                }
-                try {
-                    copyDir(source, target); //import the new world folder from the plugin's backup directory
-                } catch(IOException e) {
-                    e.printStackTrace();
-                    plugin.getLogger().severe("Failed to reset world \"" + source.getName() + "\" - could not import the " +
-                            "world from backup.");
-                    errors = true;
-                }
-                plugin.getLogger().info("Import of world \"" + source.getName() + "\" " + (errors ? "failed!" : "succeeded!"));
-                errors = false;
-            }
+        try {
+            Files.walkFileTree(Paths.get(this.plugin.getDataFolder() + template), new CopyFileVisitor(Paths.get(Bukkit.getWorldContainer() + newName)));
+        } catch(IOException e) {
+            this.plugin.getLogger().severe("Could not load world " + template);
         }
+
+        
     }
-    public void deleteWorlds() {
-        boolean worldsListed = false;
-        for(String worldName : plugin.getConfig().getStringList("random-seed.worlds")) {
-            if(!worldsListed)
-                worldsListed = true;
-            File target = new File(plugin.getServer().getWorldContainer(), worldName);
-            if(!target.exists()) {
-                plugin.getLogger().severe("Could not load world \"" + worldName + "\" with a random seed: no such world " +
-                        "exists in the server directory!");
-                return;
-            }
-            if(target.isDirectory()) {
-                if(!delete(target)) {
-                    plugin.getLogger().severe("Failed to delete world \"" + worldName + "\", perhaps the folder is locked?");
-                    continue;
-                }
-                plugin.getLogger().info("Successfully loaded a random seed for world \"" + worldName + "\"!");
-            }
+
+
+    private class CopyFileVisitor extends SimpleFileVisitor<Path> {
+        private final Path targetPath;
+        private Path sourcePath = null;
+        public CopyFileVisitor(Path targetPath) {
+            this.targetPath = targetPath;
         }
-        if(!worldsListed)
-            this.plugin.getLogger().warning("The random seed option is enabled but no worlds are listed to be deleted and " +
-                    "regenerated with random seeds.");
-    }
-    private boolean delete(File file) {
-        if(file.isDirectory())
-            for(File subfile : file.listFiles())
-                if(!delete(subfile))
-                    return false;
-        if(!file.delete())
-            return false;
-        return true;
-    }
-    private static void copyDir(File source, File target) throws IOException {
-        if(source.isDirectory()) {
-            if(!target.exists())
-                target.mkdir();
-            String files[] = source.list();
-            for(String file : files) {
-                File srcFile = new File(source, file);
-                File destFile = new File(target, file);
-                copyDir(srcFile, destFile);
+
+        @Override
+        public FileVisitResult preVisitDirectory(final Path dir, final BasicFileAttributes attrs) throws IOException {
+            if (sourcePath == null) {
+                sourcePath = dir;
+            } else {
+                Files.createDirectories(targetPath.resolve(sourcePath.relativize(dir)));
             }
-        } else {
-            InputStream in = new FileInputStream(source);
-            OutputStream out = new FileOutputStream(target);
-            byte[] buffer = new byte[1024];
-            int length;
-            //copy the file content in bytes
-            while((length = in.read(buffer)) > 0)
-                out.write(buffer, 0, length);
-            in.close();
-            out.close();
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
+            Files.copy(file, targetPath.resolve(sourcePath.relativize(file)));
+            return FileVisitResult.CONTINUE;
         }
     }
 }
