f0b9ca83d66c151e24047f8072471b0c7964fb6d
JustANull <reid.levenick@gmail.com>
2014-11-09 20:32:15 -0600

    Various kludges; Worked towards a generic GameTemplate kind of thing

--- src/main/java/net/SimpleSurvival/SimpleSurvival.java
+++ src/main/java/net/SimpleSurvival/SimpleSurvival.java
@@ -1,6 +1,10 @@
 package net.SimpleSurvival;
 
+import net.SimpleSurvival.settings.GameSettings;
+import net.SimpleSurvival.settings.GameTemplate;
 import net.SimpleSurvival.settings.WorldSettings;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
@@ -9,89 +13,173 @@ import org.bukkit.World;
 
 import java.util.HashMap;
 
+// NOTE: It should be noticed that "settings to begin a game" and "settings for a currently running game"
+// should *probably* be split, so that GameSettings doesn't have to interact with relatively static and relatively
+// active data at the same time, so we can store an array of running games and an array of possible ways to run a game
+
 /**
  * Created by maldridge on 11/8/14.
  */
 public class SimpleSurvival extends JavaPlugin {
-	CommandParser commandParser = new CommandParser();
 	SpawnManager spawnManager = new SpawnManager();
 	HashMap<String, WorldSettings> worldSettings = new HashMap<String, WorldSettings>();
+	// Map of games on particular worlds; Contains information about running/waiting games
+	// TODO: Save game settings and load them in a useful manner
+	// TODO: Remove players from the list of competitors when they disconnect
+	HashMap<String, GameTemplate> gameTemplates = new HashMap<String, GameTemplate>();
 
 	public SimpleSurvival() {
 		for(World world : getServer().getWorlds()) {
 			worldSettings.put(world.getName(), new WorldSettings());
+			// TODO: Load these from a config file, e.g.
+			// {
+			//     "castles": "backup/castles.zip",
+			//     "river": "backup/river.zip"
+			// }
+			gameTemplates.put(world.getName(), new GameTemplate(world.getName(), world.getName()));
+		}
+	}
+
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent evt) {
+		// Make sure players are not registered for events that haven't fired
+		String player = evt.getPlayer().getName();
+
+		for(GameTemplate game: gameTemplates.values()) {
+			if(game.hasCompetitor(player)) {
+				game.removeCompetitor(player);
+			}
 		}
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		return commandParser.parseCommand(sender, cmd, label, args);
-	}
-
-	private class CommandParser {
-		public boolean parseCommand(CommandSender sender, Command cmd, String label, String[] args) {
-			String cmdName = cmd.getName();
-			if(cmdName.equalsIgnoreCase("addSpawn")) {
-				return addSpawn((Player)sender);
-			} else if(cmdName.equalsIgnoreCase("changeSpawn")) {
-				if (args.length != 1) return false;
-				return setSpawn((Player)sender, args[0]);
-			} else if(cmdName.equalsIgnoreCase("getSpawns")) {
-				return getSpawns((Player)sender);
-			}
-			return false;
-		}
-
-		private boolean addSpawn(Player player) {
-			String worldName = player.getLocation().getWorld().getName();
-			boolean returnValue;
-			returnValue = spawnManager.addSpawn(worldSettings.get(worldName), player.getLocation());
-			if(returnValue == true) {
-				player.sendMessage("Successfully added spawn.");
-			}
-			return returnValue;
-		}
-
-		private boolean setSpawn(Player player, String arg) {
-			String worldName = player.getLocation().getWorld().getName();
-			int spawnNum;
-			try {
-				spawnNum = Integer.valueOf(arg);
-				--spawnNum;
-			} catch(NumberFormatException e) {
+		String cmdName = cmd.getName();
+		if(cmdName.equalsIgnoreCase("survival")) {
+			// Add the player to the list for the game they are attempting to enter
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("It is assumed that only Players may register for a game");
 				return false;
 			}
 
-			if(worldSettings.containsKey(worldName)) {
-				WorldSettings settings = worldSettings.get(worldName);
-				if(spawnNum < settings.spawns.size()) {
-					boolean returnValue;
-					returnValue = spawnManager.setSpawn(settings, spawnNum, player.getLocation());
-					if(returnValue == true) {
-						player.sendMessage("Spawn " + (spawnNum + 1) + " successfully changed.");
-					}
-					return returnValue;
-				} else {
-					player.sendMessage("Not enough spawn points already exist.");
-					return true;
-				}
+			String player = ((Player) sender).getName();
+
+			// NOTE: Assumes that games do not have a space in the name
+			if (args.length != 2) {
+				sender.sendMessage("Game un/registration takes two arguments, in the form");
+				sender.sendMessage("  register <name>");
+				sender.sendMessage("unregister <name>");
+				return false;
+			}
+
+			boolean isRegistering;
+
+			if(args[1].equalsIgnoreCase("register")) {
+				isRegistering = true;
+			} else if(args[1].equalsIgnoreCase("unregister")) {
+				isRegistering = false;
 			} else {
-				player.sendMessage("World settings do not exist.");
+				sender.sendMessage("Game registration takes 'register' or 'unregister' as the first argument");
+				return false;
+			}
+
+			String gameName = args[2];
+
+			if (!gameTemplates.containsKey(gameName)) {
+				sender.sendMessage("Could not find the game " + gameName);
+				return false;
+			}
+
+			GameTemplate game = gameTemplates.get(gameName);
+
+			if(isRegistering) {
+				if (game.hasCompetitor(player)) {
+					sender.sendMessage("You are already registered for that game");
+					return false;
+				}
+
+				for (String key : gameTemplates.keySet()) {
+					if (gameTemplates.get(key).hasCompetitor(player)) {
+						sender.sendMessage("You are already in the game " + key);
+						return false;
+					}
+				}
+
+				// Finally, there are no problems, so we can add the player to the list
+				game.addCompetitor(player);
+				sender.sendMessage("Successfully registered");
+				return true;
+			} else {
+				if (!game.hasCompetitor(player)) {
+					sender.sendMessage("You aren't registered for that game");
+					return false;
+				}
+
+				game.removeCompetitor(player);
+				sender.sendMessage("Successfully unregistered");
 				return true;
 			}
+		} else if(cmdName.equalsIgnoreCase("addSpawn")) {
+			// TODO: Add permission check
+			return addSpawn((Player)sender);
+		} else if(cmdName.equalsIgnoreCase("changeSpawn")) {
+			// TODO: Add permission check
+			if (args.length != 1) return false;
+			return setSpawn((Player)sender, args[0]);
+		} else if(cmdName.equalsIgnoreCase("getSpawns")) {
+			// TODO: Add permission check
+			return getSpawns((Player)sender);
+		}
+		return false;
+	}
+
+	private boolean addSpawn(Player player) {
+		String worldName = player.getLocation().getWorld().getName();
+		boolean success = spawnManager.addSpawn(worldSettings.get(worldName), player.getLocation());
+		if(success) {
+			player.sendMessage("Successfully added spawn.");
+		}
+		return success;
+	}
+
+	private boolean setSpawn(Player player, String arg) {
+		String worldName = player.getLocation().getWorld().getName();
+		int spawnNum;
+		try {
+			spawnNum = Integer.valueOf(arg);
+			--spawnNum;
+		} catch(NumberFormatException e) {
+			return false;
 		}
 
-		private boolean getSpawns(Player player) {
-			String worldName = player.getLocation().getWorld().getName();
+		if(worldSettings.containsKey(worldName)) {
 			WorldSettings settings = worldSettings.get(worldName);
-			int i = 1;
-			for(Integer[] spawn : settings.spawns) {
-				player.sendMessage("Spawn " + i++ + ":");
-				player.sendMessage("   x: " + spawn[0]);
-				player.sendMessage("   y: " + spawn[1]);
-				player.sendMessage("   z: " + spawn[2]);
+			if(spawnNum < settings.spawns.size()) {
+				boolean success = spawnManager.setSpawn(settings, spawnNum, player.getLocation());
+				if(success) {
+					player.sendMessage("Spawn " + (spawnNum + 1) + " successfully changed.");
+				}
+				return success;
+			} else {
+				player.sendMessage("Not enough spawn points already exist.");
+				return true;
 			}
+		} else {
+			player.sendMessage("World settings do not exist.");
 			return true;
 		}
+	}
+
+	private boolean getSpawns(Player player) {
+		String worldName = player.getLocation().getWorld().getName();
+		WorldSettings settings = worldSettings.get(worldName);
+		int i = 1;
+		for(Integer[] spawn : settings.spawns) {
+			player.sendMessage("Spawn " + i++ + ":");
+			player.sendMessage("   x: " + spawn[0]);
+			player.sendMessage("   y: " + spawn[1]);
+			player.sendMessage("   z: " + spawn[2]);
+		}
+		return true;
 	}
 }
