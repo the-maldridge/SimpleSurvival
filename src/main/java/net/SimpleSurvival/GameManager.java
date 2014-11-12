10af9e85ab955103023d273a7c5384cc4640b3d2
geppettodivacin <ericdilmore@gmail.com>
2014-11-12 00:39:00 -0600

    Fix player freeze glitchiness.
    Only cancel move events if they are translations along the x or z axes.
    Ignore any vertical movement (in case the players are spawned in the
    air) or any rotational movement.

--- src/main/java/net/SimpleSurvival/GameManager.java
+++ src/main/java/net/SimpleSurvival/GameManager.java
@@ -19,6 +19,7 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -72,7 +73,11 @@ class GameEvents implements Listener {
     public void onPlayerMove(PlayerMoveEvent event) {
         if (currentGame.getCompetitors().contains(event.getPlayer().getName())) {
             if (currentGame.getState() == GameSettings.GameState.PAUSED) {
-                event.setCancelled(true);
+                Vector to = event.getTo().toVector();
+                Vector from = event.getFrom().toVector();
+                if (to.getX() != from.getX() || to.getZ() != from.getZ()) {
+                    event.setCancelled(true);
+                }
             }
         }
     }
